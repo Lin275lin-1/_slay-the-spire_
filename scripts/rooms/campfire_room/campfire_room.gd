4ff2a92894e8710edb54f546ddee862517fbf2de
yambeam <2456927015@qq.com>
2026-04-08 10:45:20 +0800

    .

--- scripts/rooms/campfire_room/campfire_room.gd
+++ scripts/rooms/campfire_room/campfire_room.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var spine_manager: SpineManager = $SpineManager
 
+var deck_view: DeckView
 
 func _ready() -> void:
 	pass
@@ -20,4 +21,8 @@ func _on_fade_out_finished()->void :
 	pass
 
 func _on_forging_pressed() -> void:
-	pass # Replace with function body.
+	if deck_view:
+		var cards = await deck_view.select_card_pile(char_stats.deck.cards.filter(func(card: Card): return !card.upgraded), 1, 1, "选择一张牌升级")
+		if !cards.is_empty():
+			cards[0].upgrade()
+			animation_player.play("fade_out")
--- scripts/ui/top_bar.gd
+++ scripts/ui/top_bar.gd
@@ -1,7 +1,7 @@
 class_name TopBar
 extends Control
 
-signal deck_view_requested
+signal deck_view_requested(deck: Array[Card])
 
 @onready var card_pile_button: CardPileButton = $Right/Deck/CardPileButton
 @onready var avatar: TextureRect = $Left/AvatarContainer/AvatarBg/Avatar
@@ -18,7 +18,7 @@ func _ready()-> void:
 func initialize(stats: CharacterStats) -> void:
 	card_pile_button.card_pile = stats.deck
 	avatar.texture = stats.character_icon
-	card_pile_button.pressed.connect(deck_view_requested.emit)
+	card_pile_button.pressed.connect(deck_view_requested.emit.bind(stats.deck.cards))
 
 #金币更改
 func set_run_stats(new_value:RunStats)->void:
